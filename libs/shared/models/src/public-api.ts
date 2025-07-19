9030ab7cff7ae7e620a33ffaf4596e5cdedeb6cd
Manish Boge (SX/EDC2) <manish.boge@in.bosch.com>
2025-07-19 16:27:32 +0530

    feat: enhance build process and add challenge creation script
    - Updated package.json to include new build and test scripts for challenges and libraries.
    - Removed challenge list component files (HTML, SCSS, TS) as part of a refactor.
    - Added a new script (create-challenge.js) to automate the creation of new challenge applications in the monorepo.

---
+++ libs/shared/models/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of shared-models
+ */
+
+export * from './lib/product.model';
+// Add exports for other shared models here
---
+++ libs/shared/services/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of shared-services
+ */
+
+// Add exports for shared services here
