ecdfd6bdaee371e3a77399d00d7e1ecc988fe21c
Manish <manishboge@gmail.com>
2026-01-04 13:24:28 +0530

    🔧 fix(config): Update router import for consistency
    - Changed import from 'routes' to 'NGC_ROUTING_ROUTES'
    - Ensures clarity and consistency in routing configuration

--- projects/ngc-routing/src/app/app.config.ts
+++ projects/ngc-routing/src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { NGC_ROUTING_ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(NGC_ROUTING_ROUTES)]
 };
